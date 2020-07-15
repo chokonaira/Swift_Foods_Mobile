023c9602043778d36ecf32dd35b2cc104dcbd1d9
Henry Okonkwo <okonkwo.henry2012@gmail.com>
2020-07-15 14:13:50 +0200

    added background image to welcome page

--- src/screens/WelcomeScreen.jsx
+++ src/screens/WelcomeScreen.jsx
@@ -1,31 +1,48 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, Button } from 'react-native';
-import Salad from "../../assets/images/vegs.jpg"
+import { StyleSheet, Text, View, Image, ImageBackground, Button } from 'react-native';
+// import Salad from "../../assets/images/delivery-man-svg.png"
+import Salad from "../../assets/images/black-burger-banner.jpg"
+import Logo from "../../assets/images/Swift-logo.gif"
 
 
 export default function Welcome() {
   return (
-    <View style={styles.container}>
-      <Image 
-      style={styles.image}
-      source={Salad}/>
-      <Text>Hello, Welcome to CHOKO swift Foods </Text>
-    </View>
+      <ImageBackground style={styles.image} source={Salad}>
+       <View style={styles.imageText}>
+            <Image style={styles.innerImage} source={Logo}/>
+          <Text style={styles.text}>SWIFT FOODS</Text>
+        </View>
+      </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
+  text: {
+   fontSize: 35,
+   fontWeight: '900',
+   color: '#fff',
+   top: '2%'
+  },
   container: {
     flex: 1,
     width: '100%',
     height: '100%',
-    backgroundColor: '#FFffff',
-    alignItems: 'center',
+    backgroundColor: 'green',
+    alignItems: "center",
     justifyContent: 'center',
   },
+  innerImage: {
+    borderRadius: 100
+   },
   image: {
     width: '100%',
-    height: '50%',
-    borderRadius: 10
+    height: '100%',
+    flex: 1 
+  },
+  imageText: {
+    alignItems: 'center', 
+    justifyContent: 'flex-start',
+    top: '5%',
+    flex: 1,
   }
 });

ce1853d1bf0c27c04ea873790094871988b03e17
Felipe Sere <felipesere@gmail.com>
2020-10-14 09:50:07 +0200

    This at least makes the tests "run"?

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,12 @@
 module.exports = function (api) {
-  api.cache(true);
+  api.cache(false);
   return {
-    presets: ["babel-preset-expo", 'module:metro-react-native-babel-preset', 'module:react-native-dotenv'],
+    presets: [
+      "@babel/preset-env",
+      "@babel/preset-react",
+      "babel-preset-expo",
+      "module:metro-react-native-babel-preset",
+      // "module:react-native-dotenv",
+    ]
   };
 };
